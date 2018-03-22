57f98d9dd36b22ee80bd1d9d268d1eb89fde44a4
dmitriy <kulikov.perm@gmail.com>
2018-03-23 00:53:13 +0300

    Refactor main and Victorine class

--- lib/victorine.rb
+++ lib/victorine.rb
@@ -1,16 +1,19 @@
 class Victorine
-  attr_reader :questions_all, :time_all
+  attr_reader :questions_all
+  attr_accessor :time_all, :late_answers_count, :incorrect_answers_count, :correct_answers_count
 
-  def initialize(doc)
-    parse_doc(doc)
+  def initialize(questions_all)
+    @questions_all = questions_all
     @time_all = 0
     @correct_answers_count = 0
     @incorrect_answers_count = 0
     @late_answers_count = 0
   end
 
-  def parse_doc(doc)
-    @questions_all = []
+  # Парсим xml-документ, получаем многомерный хэш, где ключ - это вопрос,
+  # а значение хэш, с ответами
+  def self.parse_doc(doc)
+    questions_all = []
     doc.root.elements.each("question") do |q|
       questions, answers = {},{}
       questions[:question] = q.elements["title"].text
@@ -23,50 +26,29 @@ class Victorine
       end
 
       questions[:answers] = answers
-      @questions_all << Question.new(questions)
+      questions_all << Question.new(questions)
       questions, answers = {}, {}
     end
+    new(questions_all)
   end
 
-  def run
-    questions_all.each do |q|
-      puts "Вопрос: #{q.question}"
-      puts "Время на ответ: #{q.seconds_to_answer} сек."
-      puts "Варианты ответов: \n\r#{q.answers_text}"
-
-      time_start = Time.now
-
-      begin
-        puts "Введите номер ответа от 1 до #{q.answers.size}: "
-        user_answer = STDIN.gets.to_i
-      end until user_answer > 0 && user_answer <= q.answers.size
-
-      time_end = Time.now
-
-      @time_all += (time_end - time_start)
-
-      if q.seconds_to_answer.to_i <= (time_end - time_start).to_i
-        puts "Время на ответ истелко. Ответ не засчитан"
-        @late_answers_count += 1
-        next
-      else
-        if q.answers.keys[user_answer - 1] == q.correct_answer.to_s
-          puts "Верно"
-          puts
-          @correct_answers_count += 1
-        else
-          puts "Не верно. Правильный ответ: #{q.correct_answer}"
-          @incorrect_answers_count += 1
-        end
-      end
-    end
+  # Только один вопрос.
+  def ask_question(current_question)
+      puts "Вопрос: #{current_question.question}"
+      puts "Время на ответ: #{current_question.seconds_to_answer} сек."
+      puts "Варианты ответов: \n\r#{current_question.answers_text}"
   end
 
+  # Результат викторины
   def result
-    correct_answers_result = "\tПравильных ответов: #{@correct_answers_count}\n\r"
-    incorrect_answers_result = "\tНе правильных ответов: #{@incorrect_answers_count}\n\r"
-    time_spent_result = "\tПотрачено на тест: #{@time_all.round(2)} сек.\n\r"
-    late_answers_result = "\tОтветы на которые превышено время: #{@late_answers_count}"
-    correct_answers_result + incorrect_answers_result + time_spent_result + late_answers_result
+    result = ""
+    correct_answers_result = "\tПравильных ответов: #{correct_answers_count}\n\r"
+    result << correct_answers_result
+    incorrect_answers_result = "\tНе правильных ответов: #{incorrect_answers_count}\n\r"
+    result << incorrect_answers_result
+    time_spent_result = "\tПотрачено на тест: #{time_all.round(2)} сек.\n\r"
+    result << time_spent_result
+    late_answers_result = "\tОтветы на которые превышено время: #{late_answers_count}"
+    result << late_answers_result
   end
 end
--- quiz.rb
+++ quiz.rb
@@ -8,13 +8,47 @@ abort "File not found" unless File.exist?(file)
 
 doc = REXML::Document.new(file)
 
-victorine = Victorine.new(doc)
+victorine = Victorine.parse_doc(doc)
 
-victorine.run
+# Здесь должны быть обьекты класса Question
+questions_all = victorine.questions_all
+
+questions_all.each do |current_question|
+  victorine.ask_question(current_question)
+
+  time_start = Time.now
+
+  user_answer = ""
+  loop do
+    puts "Введите номер ответа от 1 до #{current_question.answers.size}: "
+    user_answer = STDIN.gets.to_i
+    break if user_answer > 0 && user_answer <= current_question.answers.size
+  end
+
+  time_end = Time.now
+
+  victorine.time_all += (time_end - time_start)
+
+  if current_question.seconds_to_answer.to_i <= (time_end - time_start).to_i
+    puts "Время на ответ истелко. Ответ не засчитан"
+    victorine.late_answers_count += 1
+    next
+  else
+    if current_question.answers.keys[user_answer - 1] == current_question.correct_answer.to_s
+      puts "Верно"
+      puts
+      victorine.correct_answers_count += 1
+    else
+      puts "Не верно. Правильный ответ: #{current_question.correct_answer}"
+      victorine.incorrect_answers_count += 1
+    end
+  end
+end
 
 puts
 puts "Рассчитываем результат: "
 result = victorine.result
+
 sleep 2
 
 puts result
